8ae4c7078d40139c826887a488f18c5428a34548
GayathriAlavala2110 <gayathrialavala2007@gmail.com>
2026-09-15 11:23:42 +1200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify, send_from_directory
 import json
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=None)
 
 
 # ================= HOME =================
